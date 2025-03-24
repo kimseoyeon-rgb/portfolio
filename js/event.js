a5bea96a5b608dc198675b8bc99368267dd6d5b0
kimseoyeon-rgb <ksyeon112350@gmail.com>
2025-03-24 13:51:59 +0900

    0324-1

--- js/event.js
+++ js/event.js
@@ -1,11 +1,28 @@
 $(function () {
     $("#slide-open").click(function() {
-        if("#burgur".hasClass('on')) {
-            $("#burgur").removeClass('on');
-            // $("#slide").removeClass('on');
+        if($(".burgur").hasClass("on")) {
+            $(".burgur").removeClass("on");
+            $(".menu").removeClass("on")
         } else {
-            $("#burgur").addClass('on');
-            // $('#slide').addClass("on");
+            $(".burgur").addClass("on");
+            $(".menu").addClass("on");
         }
     });
-});
+
+    $("#firstMenu").click(function() {
+        $(".burgur").removeClass("on");
+        $(".menu").removeClass("on");
+    })
+    $("#secondMenu").click(function() {
+        $(".burgur").removeClass("on");
+        $(".menu").removeClass("on");
+    })
+    $("#thirdMenu").click(function() {
+        $(".burgur").removeClass("on");
+        $(".menu").removeClass("on");
+    })
+    $("#fourthMenu").click(function() {
+        $(".burgur").removeClass("on");
+        $(".menu").removeClass("on");
+    })
+})
